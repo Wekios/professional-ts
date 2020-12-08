00e1c509238f4b8cbccc2719d868855c8a439312
Veljko Blagojevic <veljkod.blagojevic@gmail.com>
2020-12-08 21:57:49 +0100

    Phase II: no implicit any

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -2,28 +2,28 @@
 
 /**
  * Stringify an Error instance
- * @param {Error} err - The error to stringify
- * @return {string}
+ * @param err - The error to stringify
  */
-function stringifyErrorValue(err) {
+function stringifyErrorValue(err: Error): string {
   return `${err.name.toUpperCase()}: ${err.message}
   ${err.stack || '(no stack trace information)'}`;
 }
 
 /**
  * Stringify a thrown value
- *
- * @param {string} errorDescription
- * @param {any} err
- *
- * @return {string}
+ * @param errorDescription
+ * @param err
  */
-export function stringifyError(errorDescription, err) {
+export function stringifyError(
+  errorDescription: string,
+  err: any,
+): string {
   return `${errorDescription}\n${
     err instanceof Error
       ? stringifyErrorValue(err)
       : err
-      ? '' + err
+      ? // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
+        '' + err
       : '(missing error information)'
   }`;
 }
